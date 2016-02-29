03e3f196e5b91e9a4d8dea70c479bdad5110e8ad
Pierre <pierre.mar.66000@gmail.com>
2016-02-29 21:03:08 +0100

    all works again

--- Settings/Routes.php
+++ Settings/Routes.php
@@ -4,4 +4,4 @@
 Route::addRoute('GET', 'login', 'login');
 Route::addRoute('POST', 'login', 'login')->addPOST('email')->addPOST('password');
 Route::addRoute('GET', 'dashboard', 'dashboard');
-Route::addRoute('GET', 'login/@tamo/@test/aie', 'index')->using("@tamo", "^[1-9]*$")->using("@test", "^[a-z]*$");
+Route::addRoute('GET', 'login/@tamo/@test/aie/@osef', 'index')->using("@tamo", "^[1-9]*$")->using("@test", "^[a-z]*$");
