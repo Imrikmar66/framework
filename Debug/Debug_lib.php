5b85cac46784d5e430cfec4b467cbbab97308b73
= <imacagence2@rnative.home>
2016-11-10 12:17:51 +0100

    - Adding Controller::getModuleUrl, Controller::getModulePath       smarty templates implement {URL_ASSETS} for module/name/assets folder - Adding Debug autoload for mod debug

--- Debug/Debug_lib.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once __DIR__.'/Debug.php';
-require_once __DIR__.'/Debug_modules.php';
--- Settings/header.php
+++ Settings/header.php
@@ -3,7 +3,7 @@ require_once 'config.php';
 if(MODE_DEV == TRUE){ 
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-    require_once URI_FOLDER.'/Debug/Debug_lib.php';
+    require_once URI_FRAMEWORK.'/Debug/Debug_autoloader.php';
 }
 
 require_once URI_FRAMEWORK.'/Context.php';
