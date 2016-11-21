c902085fae24ee937940c274da7e29caf76a9616
Jordan B <konfuciuz@gmail.com>
2016-11-21 11:05:35 +0100

    modified install command (abort if db settings are already defined)

--- Framework/Tools/console/console.php
+++ Framework/Tools/console/console.php
@@ -1,13 +1,16 @@
 <?php
 require_once "External/vendor/autoload.php";
 require_once 'Command/CreateModuleCommand.php';
+require_once 'Command/InstallCommand.php';
 
 use Symfony\Component\Console\Application;
 use AppBundle\Command\CreateModuleCommand;
+use AppBundle\Command\InstallCommand;
 
 
 $application = new Application();
 
+$application->add(new InstallCommand());
 $application->add(new CreateModuleCommand());
 
 $application->run();
